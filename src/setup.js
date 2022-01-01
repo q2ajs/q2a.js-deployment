935cad2002776fc88c5850020bd1efb40ab40e9d
hpnarges <hpnarges8@gmail.com>
2022-01-01 10:22:45 +0330

    fix frontend .env

--- src/setup.js
+++ src/setup.js
@@ -184,7 +184,7 @@ const createDockerComposerFromConfigs = (sampleConfig, dockerSettingFileNames) =
       createEnvFromSettingsJson(
         `${configsDirectory}/${siteName}.frontend.deploy_settings.json`,
         `${outputDirectory}/${siteName}/frontend/.env`,
-        [{ NEXT_PUBLIC_GRAPHQL_URL: `http://api:${dockerSettings.API_PORT.defaultValue}/graphql` }]
+        [{ NEXT_PUBLIC_GRAPHQL_URL: `http://${siteName}_api:4000/graphql` }]
       );
     }
   }
